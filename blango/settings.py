f372cf43cb1ee605aff12deef7446b2bdb8ea938
Ab3nXY <127937158+Ab3nXY@users.noreply.github.com>
2023-11-09 22:16:27 +0300

    Update settings.py with secure configuration

--- blango/settings.py
+++ blango/settings.py
@@ -15,7 +15,7 @@ from configurations import Configuration
 from configurations import values
 import dj_database_url
 from datetime import timedelta
-
+from decouple import config
 
 
 
@@ -29,7 +29,7 @@ class Dev(Configuration):
     # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
     # SECURITY WARNING: keep the secret key used in production secret!
-    SECRET_KEY = 'django-insecure-+sn%dpa!086+g+%44z9*^j^q-u4n!j(#wl)x9a%_1op@zz2+1-'
+    SECRET_KEY = config('SECRET_KEY', default='your_default_secret_key')
 
     # SECURITY WARNING: don't run with debug turned on in production!
     DEBUG = True
